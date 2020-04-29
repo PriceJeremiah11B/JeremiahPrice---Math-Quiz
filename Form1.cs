26f4a73d3c09a51a27bac0bee1b606e3fe13440b
Jeremiah Price <JeremiahP94@outlook.com>
2020-04-29 12:37:13 -0600

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeremiahPrice___Math_Quiz
+{
+    public partial class Form1 : Form
+    {
+
+        Random randomizer = new Random();
+
+        int addend1;
+        int addend2;
+
+        public void StartTheQuiz()
+        {
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+           
+
+            sum.Value = 0;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
